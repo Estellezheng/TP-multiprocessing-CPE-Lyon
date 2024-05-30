8f2a056dbd9be4cf048e5a3a451258f45c91adcd
Estellezheng <estelle.zheng@outlook.fr>
2024-05-30 12:07:56 +0200

    aa

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# microprocessing
+
--- TP4/exo4.py
+++ TP4/exo4.py
@@ -10,4 +10,44 @@ Deux processus P1 et P2 souhaitent établir un rendez-vous avant l’exécution 
 rdv2() pour l’autre. En utilisant les sémaphores, écrire les scripts P1 et P2 permettant d’établir ce rendez-vous.
  Rendez-vous à 2 et à 3 : Réaliser un rendez-vous entre 3 processus P1, P2 et P3."""
 
-a
+
+def process1 (N) : 
+    print("process1")
+
+
+def producteur2 (N) : 
+    for i in range (N):
+        val = random.randint(0,9)    
+        Q2.put(val)
+        print("2", val)
+        
+
+
+
+if __name__ == "__main__" : 
+
+    N = 10
+    rdv_c1 = mp.Semaphore(0)  # creation semaphore initilalisé à 1
+    rdv_c2 = mp.Semaphore(0)
+    
+    p1 = mp.Process(target = producteur1, args=(N,))
+    p2 = mp.Process(target = producteur2, args=(N,))
+
+
+    Q1 = mp.Queue()
+    Q2 = mp.Queue()
+
+    p1.start()
+    p2.start()
+    c1.start()
+    c2.start()
+
+    p1.join()
+    p2.join()
+    c1.join()
+    c2.join()
+
+    
+    sys.exit(0)
+
+
