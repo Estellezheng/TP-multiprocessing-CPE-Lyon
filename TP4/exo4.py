9ac99ef6dfcd6760632ddf858edd9ce28bb7631b
Estellezheng <estelle.zheng@outlook.fr>
2024-05-30 12:07:38 +0200

    a

--- TP4/exo4.py
+++ TP4/exo4.py
@@ -10,4 +10,4 @@ Deux processus P1 et P2 souhaitent établir un rendez-vous avant l’exécution 
 rdv2() pour l’autre. En utilisant les sémaphores, écrire les scripts P1 et P2 permettant d’établir ce rendez-vous.
  Rendez-vous à 2 et à 3 : Réaliser un rendez-vous entre 3 processus P1, P2 et P3."""
 
-
+a
