e8af7dbbda15cc7a387b19ec390926a0c7f1ecab
Estellezheng <estelle.zheng@outlook.fr>
2024-05-30 12:01:37 +0200

    exo4

---
+++ TP4/exo4.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import multiprocessing as mp
+import time
+import random
+
+
+"""Rendez-vous à 2 et à 3 
+Deux processus P1 et P2 souhaitent établir un rendez-vous avant l’exécution de la fonction rdv1() pour l’un et
+rdv2() pour l’autre. En utilisant les sémaphores, écrire les scripts P1 et P2 permettant d’établir ce rendez-vous.
+ Rendez-vous à 2 et à 3 : Réaliser un rendez-vous entre 3 processus P1, P2 et P3."""
+
+
